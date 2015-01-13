96f7151d0729124e93837c960ddc1229a169f940
mnbvcxz075 <x170gbc9j346yv2tbj7z@gmail.com>
2015-01-14 01:19:01 +0900

    test

--- ConsoleApplication2/ConsoleApplication2/HandRecognition.cpp
+++ ConsoleApplication2/ConsoleApplication2/HandRecognition.cpp
@@ -9,7 +9,7 @@ lower( cv::Scalar(0, 0, 0, 0))
 
 }
 HandRecognition::HandRecognition(cv::VideoCapture cap)
-: upper(cv::Scalar(255 255, 255, 255)),
+: upper(cv::Scalar(255, 255, 255, 255)),
 lower(cv::Scalar(100, 125, 110, 0))
 {
 	this->capture = cap;
